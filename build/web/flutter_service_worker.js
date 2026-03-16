e2be4f2f2abbdc894b2e7972a6cc49fdc7828974
Dev Moss <1DEV04l@example.com>
2026-03-16 10:56:14 +0700

    fix: _uploadToCloudinary accept dynamic for web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,14 +28,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "66fac67cc9bfcbe0ba03c10a9d6bbee6
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "44b6f3e765d55bf43e4adb04b4a0d1f9",
+"flutter_bootstrap.js": "6d5ba621b711b78e0736ad61786c7f55",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "acfffd1a083ede0eda1ab3d5f3f26ff8",
 "/": "acfffd1a083ede0eda1ab3d5f3f26ff8",
-"main.dart.js": "3a424d376b468ef43fee9e53ff864a9b",
+"main.dart.js": "54be214cf0926b90a0ff988e2165766d",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"};
 // The application shell files that are downloaded before a service worker can
